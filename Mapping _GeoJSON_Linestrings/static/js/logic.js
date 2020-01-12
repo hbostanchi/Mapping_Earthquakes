9e3141da859240b124999133432a5421cae1101b
hbostanchi <42923135+hbostanchi@users.noreply.github.com>
2020-01-11 16:50:33 -0800

    torontoRoutes

--- Mapping _GeoJSON_Linestrings/static/js/logic.js
+++ Mapping _GeoJSON_Linestrings/static/js/logic.js
@@ -1,39 +1,53 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+
+
+// We create the light tile layer that will be the background of our map.
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	id: 'mapbox.streets',
 	accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-// Coordinates for each point to be used in the polyline.
-let line = [
-	[33.9416, -118.4085],
-	[37.6213, -122.3790],
-	[40.7899, -111.9791],
-	[47.4502, -122.3088]
-  ];
-  // Create a polyline using the line coordinates and make the line red.
-	L.polyline(line, {
-	color: "yellow"
-  }).addTo(map);
 
-  // Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: [8, 8],
-    dashOffset: "1"
-}).addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
+// Create a base layer that holds both maps.
+baseMaps = {
+    DayNavigation: day,
+    NightNavigation: night
+};
 
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [day]
+});
 
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
+// Accessing the Toronto airline routes GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/hbostanchi/Mapping_Earthquakes/master/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: '#ffffa1',
+    weight: 2
+};
+
+// Grabbing our GeoJSON Data
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+        }
+    }).addTo(map);
+});
